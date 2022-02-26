d6f5ee4ff35daa34b401b1c9932f5a390f559325
Rolf Michelsen <rolf@rolfmichelsen.com>
2022-02-26 19:22:12 +0100

    Simplify styling of app footer
    Remove one level of div and styling.

--- app/src/component/footer.tsx
+++ app/src/component/footer.tsx
@@ -27,21 +27,19 @@ export default function FooterPanel(): JSX.Element
 {
     return (
         <div className="footer shadedcontainer">
-            <div className="footerInfo">
-                <div className="footerInfoPanelLeft">
-                    <p className="footerInfoStrong">Space Cowboy</p>
-                    <p>the place in space for fast decisions and great collaboration on the world wild web and the galaxy</p>
-                    <p></p>
-                    <p>No warranty. No personal data collected.</p>
-                    <p className="copyrightnotice">&copy; Copyright 2021 Rolf Michelsen and Tami Weiss<br/>
-                                                   Version: {Configuration.Version}</p>
-                </div>
-                <div className="footerInfoPanelRight">
-                    <p className="footerInfoStrong">Send feedback</p>
-                    <p>Space Cowboy HQ</p>
-                    <p>OSLO NORWAY</p>
-                    <p><a className="emaillink" href="mailto:howdy@spacecowboy.app">howdy@spacecowboy.app</a></p>
-                </div>
+            <div className="footerInfoPanelLeft">
+                <p className="footerInfoStrong">Space Cowboy</p>
+                <p>the place in space for fast decisions and great collaboration on the world wild web and the galaxy</p>
+                <p></p>
+                <p>No warranty. No personal data collected.</p>
+                <p className="copyrightnotice">&copy; Copyright 2021 Rolf Michelsen and Tami Weiss<br/>
+                                                Version: {Configuration.Version}</p>
+            </div>
+            <div className="footerInfoPanelRight">
+                <p className="footerInfoStrong">Send feedback</p>
+                <p>Space Cowboy HQ</p>
+                <p>OSLO NORWAY</p>
+                <p><a className="emaillink" href="mailto:howdy@spacecowboy.app">howdy@spacecowboy.app</a></p>
             </div>
         </div>
     );
