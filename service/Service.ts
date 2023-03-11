3ada60273fea183cb27d680d3712948cc4a5a3d3
Rolf Michelsen <rolf@rolfmichelsen.com>
2023-03-11 16:04:58 +0000

    Add service access class
    Add class Service encapsulating an interface to the service
    component.  Implement function to retrieve a random session name.

---
+++ service/Service.ts
@@ -0,0 +1,40 @@
+/*
+    Copyright 2021-2023 Rolf Michelsen and Tami Weiss
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+import Configuration from "@/Configuration";
+import log from "loglevel";
+
+import ServiceException from "./ServiceException";
+
+/** Access the Spacecowboy service component. */
+export default class Service 
+{
+    private static readonly headers = {
+        "User-Agent": `Spacecowboy/${Configuration.AppVersion}`,
+        "Content-Type": "application/json"
+    }
+
+    /** Return a random session identifier. */
+    public static async GetRandomSessionId(): Promise<string>
+    {
+        const response = await fetch(`${Configuration.ApiBase}/api/v0/session/random`, {method: "GET", headers: this.headers});
+        log.debug(`GetRandomSessionId returned ${response.status}`)
+        if (response.ok) {
+            return response.json();
+        }
+        throw new ServiceException(response.status, response.statusText);
+    }
+}
