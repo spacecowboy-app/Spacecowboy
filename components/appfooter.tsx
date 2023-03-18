11214aef2add88a39a8129a6f8fc23a867bbfa42
Rolf Michelsen <rolf@rolfmichelsen.com>
2023-03-18 21:45:58 +0000

    Move GitHub link to the right part of the footer

--- components/appfooter.tsx
+++ components/appfooter.tsx
@@ -33,13 +33,8 @@ export default function ApplicationFooter(): JSX.Element
                 <Typography variant="h4">Space Cowboy</Typography>
                 <Typography pt={1}>The place in space for fast decisions and great collaboration on the world wild web and the galaxy</Typography>
                 <Typography pt={1}>No warranty. No personal data collected.</Typography>
-                <Typography variant="subtitle1" pt={1}>Copyright 2021-2023 Rolf Michelsen and Tami Weiss</Typography>
-                <Typography variant="subtitle1">Version {Configuration.AppVersion}</Typography>
-            </Box>
-            <Box alignSelf="flex-end">
-                <Link href="https://github.com/spacecowboy-app" >
-                    <Image src={Github} alt="Github repository" />
-                </Link>
+                <Typography variant="subtitle1" pt={1} pb={0}>Copyright 2021-2023 Rolf Michelsen and Tami Weiss</Typography>
+                <Typography variant="subtitle1" pt={0}>Version {Configuration.AppVersion}</Typography>
             </Box>
             <Box pr={2}>
                 <Typography variant="h4">Send feedback</Typography>
@@ -48,6 +43,11 @@ export default function ApplicationFooter(): JSX.Element
                 <Link href="mailto:howdy@spacecowboy.app">
                     <Typography pt={1}>howdy@spacecowboy.app</Typography>
                 </Link>                    
+                <Box pt={0.8}>
+                    <Link href="https://github.com/spacecowboy-app" >
+                        <Image src={Github} alt="Github repository" width={32} />
+                    </Link>
+                </Box>
             </Box>
         </Stack>
     );
