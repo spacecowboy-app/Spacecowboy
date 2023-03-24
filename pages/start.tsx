3389512a9d40c51f0d8ea41b1e6f3125afccca81
Rolf Michelsen <rolf@rolfmichelsen.com>
2023-03-24 21:32:10 +0000

    Validate session id with error message
    Decorate the TextField with a descriptive error message when the
    session id is invalid.

--- pages/start.tsx
+++ pages/start.tsx
@@ -31,7 +31,7 @@ import HeroImage from "@/images/hero/place.png";
 
 interface Session {
     id: string,
-    isValid: boolean,
+    error?: string,
 };
 
 
@@ -46,7 +46,7 @@ export default function StartGame(): JSX.Element {
             Service.GetRandomSessionId()
             .then(result => { 
                 log.debug(`Got random session name ${result}`);
-                setSession({ id: result, isValid: isSessionIdValid(result) });
+                setSession({ id: result, error: validateSessionId(result) });
             })
             .catch(() => log.error("Failed to get session name from service"));
         }
@@ -57,7 +57,7 @@ export default function StartGame(): JSX.Element {
             <Stack spacing={2} alignItems="center">
                 <Image src={HeroImage} alt="Welcome to Spacecowboy" />
                 <Typography variant="h3">Name your space or take one here</Typography>
-                <TextField id="session-id" value={session?.id ?? ""} error={!session?.isValid} autoFocus={true} onChange={(e: React.ChangeEvent<HTMLInputElement>) => updateSessionId(e)} />
+                <TextField id="session-id" value={session?.id ?? ""} error={session?.error !== undefined} label={session?.error} autoFocus={true} onChange={(e: React.ChangeEvent<HTMLInputElement>) => updateSessionId(e)} />
                 <Button variant="contained" type="submit" >take this place</Button>
             </Stack>
         </Box>
@@ -67,22 +67,28 @@ export default function StartGame(): JSX.Element {
     function updateSessionId(e: React.ChangeEvent<HTMLInputElement>): void 
     {
         const id = e.target.value.trim();
-        setSession({ id: id, isValid: isSessionIdValid(id) });
+        setSession({ id: id, error: validateSessionId(id) });
     }
 
 
-    /** Return `true` is the session id is valid. */
+    /** Return an error message if the session id is invalid, otherwise `undefined`. */
     // TODO: Implement check for session name already in use
-    function isSessionIdValid(id: string): boolean
+    function validateSessionId(id: string): string|undefined
     {
-        if (id.length > 50 || RegExp("[/&#?]").test(id))
-            return false;
+        if (id.length == 0)
+            return "Please provide space name"
+
+        if (id.length > 50)
+            return "Name too long";
+
+        if (RegExp("[/&#?]").test(id))
+            return "Name contains invalid characters"
         
         const reservedIds = [ "about", "join", "start" ];
         if (reservedIds.find(e => e == id.toLowerCase()))
-            return false;
+            return "Name is reserved";
 
-        return true;
+        return undefined;
     }
 
 
