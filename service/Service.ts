0edc462e0f3f9bc0b43e404aeaec09d40d5f5ec8
Rolf Michelsen <rolf@rolfmichelsen.com>
2023-05-21 13:59:24 +0000

    Add service methods GetSession and CreateSession

--- service/Service.ts
+++ service/Service.ts
@@ -21,6 +21,9 @@ import log from "loglevel";
 import charms from "@/charms";
 import decks from "@/decks";
 import ServiceException from "./ServiceException";
+import Session from "@/model/Session";
+import { asSession } from "@/service/dto/SessionResponse";
+
 
 /** Access the Spacecowboy service component. */
 export default class Service
@@ -72,4 +75,42 @@ export default class Service
     {
         return new Promise((resolve) => resolve(decks));
     }
+
+
+    /**
+     * Get information about a session
+     * @param id  Session identifier
+     * @param participantId ID of participant requesting the information
+     * @throws {ServiceException} Error in communicating with the service
+     * @returns A session object or null if the session does not exist
+     */
+    public static async GetSessionAsync(id: string, participantId?: string): Promise<Session|null>
+    {
+        const pquery = participantId ? `?participantId=${participantId}` : "";
+        const response = await fetch(`${Configuration.ApiBase}/api/v0/session/${id}${pquery}`, {method: "GET", headers: this.headers});
+        if (!response.ok) {
+            if (response.status === 404) {
+                return null;
+            }
+            throw new ServiceException(response.status, response.statusText);
+        }
+        return asSession(await response.json());
+    }
+
+
+    /**
+     * Create a new session
+     * The session is identified by a session identifier, which must be unique.
+     * @param sessionId Session identifier
+     * @throws {ServiceException} Error in communicating with the service
+     */
+    public static async CreateSessionAsync(sessionId: string): Promise<Session>
+    {
+        const response = await fetch(`${Configuration.ApiBase}/api/v0/session/${sessionId}`, {method: "PUT", headers: this.headers});
+        if (response.ok) {
+            return asSession(await response.json());
+        }
+        throw new ServiceException(response.status, response.statusText);
+    }
+
 }
