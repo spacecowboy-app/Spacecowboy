9e38b1fc39b64b87d7fce430630d2eba0c8d7348
Rolf Michelsen <rolf@rolfmichelsen.com>
2023-07-20 11:24:21 +0000

    Start of deck gallery component
    This component renders a set of cards, suitable for both
    presenting a deck and a set of deck tops.  It provides a
    callback when a card is selected.

---
+++ components/DeckGallery.tsx
@@ -0,0 +1,47 @@
+/*
+    Copyright 2021-2023 Rolf Michelsen and Tami Weiss
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+import React, { useEffect, useState } from "react";
+
+import Grid from '@mui/material/Unstable_Grid2';
+
+import log from "loglevel";
+
+import Card from "./Card";
+import CardModel from "@/model/Card";
+
+
+interface Props {
+
+    /** Cards to show in the gallery. */
+    cards: CardModel[],
+
+    /** Callback handling selection of a specific card.  The parameter is the selected card id. */
+    selectCard?: (id: string) => void
+};
+
+
+/**
+ * Presents a set of cards.
+ */
+export default function DeckGallery(props: Props): JSX.Element
+{
+    return (
+        <Grid container spacing={2} >
+            { props.cards.map(c => <Card key={c.id} card={c} handleClick={() => { if (props.selectCard) props.selectCard(c.id); }} /> ) }
+        </Grid>
+    );
+}
