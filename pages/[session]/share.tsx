a5f19f013f247db914f9634c50219a4b85670610
Rolf Michelsen <rolf@rolfmichelsen.com>
2023-07-24 14:38:54 +0000

    Add page to share session link
    Show the session URL in a read-only field and a button
    to copy the link to the clipboard.  Opens a snackbar
    when the button is clicked and then redirects back to
    the session.  This is a bit hard to see and takes a
    while so might need some improvement...

---
+++ pages/[session]/share.tsx
@@ -0,0 +1,83 @@
+/*
+    Copyright 2021-2023 Rolf Michelsen and Tami Weiss
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+import React, { useState, SyntheticEvent } from "react";
+
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import Snackbar, { SnackbarCloseReason } from "@mui/material/Snackbar";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+
+import { useRouter } from "next/router";
+
+import log from "loglevel";
+
+import Constants from "@/constants";
+import HeroImage from "@/components/HeroImage";
+
+import heroImage from "@/images/hero/place.png";
+
+
+
+/**
+ * Page for sharing a session with others.
+ */
+export default function ShareSessionPage(): JSX.Element
+{
+    const [ infoOpen, setInfoOpen ] = useState<boolean>(false);
+    const router = useRouter();
+    const sessionId = router.query.session as string;
+    const sessionLink = `${document.location.origin}/${sessionId}`;
+
+    return (
+        <>
+            <Stack spacing={2} alignItems="center">
+                <HeroImage src={heroImage} alt="" />
+                <Typography variant="h1">Share your place</Typography>
+                <Typography variant="h3">Invite more Space Cowboys</Typography>
+                <Typography variant="h3">Copy, paste and send the link</Typography>
+                <TextField value={sessionLink} />
+                <Button variant="contained" onClick={handleButtonClick} >Copy link</Button>
+            </Stack>
+            <Snackbar open={infoOpen} autoHideDuration={Constants.SnackbarDuration} onClose={handleInfoClose} anchorOrigin={Constants.SnackbarAnchor}>
+                <Alert severity="info" >
+                    Place name copied to clipboard.
+                </Alert>
+            </Snackbar>
+        </>
+    );
+
+    function handleButtonClick(): void
+    {
+        navigator.clipboard.writeText(sessionLink);
+        setInfoOpen(true);
+        log.info(`Copied session link to clipboard: ${sessionLink}`);
+    }
+
+
+    /** Callback for closing the error snackbar. */
+    function handleInfoClose(event: SyntheticEvent<any, Event>|Event, reason: SnackbarCloseReason): void
+    {
+        if (reason == "clickaway") {
+            return;
+        }
+        setInfoOpen(false);
+        router.push({ pathname:"/[session]", query: { session: sessionId } });
+    }
+
+}
