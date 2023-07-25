af51c11dbbc9417fb14d95079bc5286149795fd6
Rolf Michelsen <rolf@rolfmichelsen.com>
2023-07-25 09:41:55 +0000

    Move deck selection to dedicated component
    Introduce the DeckSelector component to handle
    deck selection, simplifying DeckCreator.

--- components/DeckCreator.tsx
+++ components/DeckCreator.tsx
@@ -18,13 +18,12 @@ import React, { useEffect, useState } from "react";
 
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Select from "@mui/material/Select";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import { MenuItem } from "@mui/material";
 
 import log from "loglevel";
 
+import DeckSelector from "./DeckSelector";
 import Deck from "@/model/Deck";
 import DeckCategoryFilter from "@/model/DeckCategoryFilter";
 import { getDecksAsync } from "@/service/Service";
@@ -42,7 +41,6 @@ export default function DeckCreator(props: Props): JSX.Element
 {
     const [decks, setDecks] = useState<Deck[]|undefined>();     // All available decks
     const [deckFilters, setDeckFilters] = useState<DeckCategoryFilter[]|undefined>();       // deck category filters
-    const [selectedDeckCategory, setSelectedDeckCategory] = useState<DeckCategoryFilter|undefined>();   // currently selected deck type
     const [selectedDeck, setSelectedDeck] = useState<Deck|undefined>();                                 // currently selected deck
 
     /* Get all available decks. */
@@ -52,7 +50,6 @@ export default function DeckCreator(props: Props): JSX.Element
                 .then(result => {
                     setDecks(result.decks);
                     setDeckFilters(result.deckFilters);
-                    setSelectedDeckCategory(result?.deckFilters ? result.deckFilters[0] : undefined);
                     log.debug(`Got ${result.decks.length} decks.`);
                 })
                 .catch(() => log.error("Unable to get any decks."));
@@ -66,20 +63,9 @@ export default function DeckCreator(props: Props): JSX.Element
     /* If no deck has been selected, show all decks and let the player select a deck. */
     if (!selectedDeck) {
         return (
-            <Box>
-                <Stack spacing={2} alignItems="center">
-                    <Typography variant="h1">select deck</Typography>
-                    { deckFilters && selectedDeckCategory &&
-                        <Select value={selectedDeckCategory.name} onChange={(e) => { setSelectedDeckCategory(deckFilters.find(f => f.name == (e.target.value as string))); setSelectedDeck(undefined); }}>
-                            { deckFilters.map(d => <MenuItem key={d.name} value={d.name}>{d.name}</MenuItem>) }
-                        </Select>
-                    }
-                    <DeckGallery cards={decks.filter(d => !selectedDeckCategory || selectedDeckCategory.decks.some(id => id == d.id)).map(d => d.decktop)} selectCard={(id) => selectDeck(id)} />
-                </Stack>
-            </Box>
+            <DeckSelector decks={decks} deckFilters={deckFilters} deckSelected={(id) => selectDeck(id)} />
         );
     }
-
 
     /* A deck has been selected.  Show the deck cards and allow for customization. */
     if (selectedDeck?.cards) {
