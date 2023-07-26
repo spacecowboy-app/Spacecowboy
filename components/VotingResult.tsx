98cc07d09aff236696eb1e100c46f3fa22994289
Rolf Michelsen <rolf@rolfmichelsen.com>
2023-07-26 12:16:52 +0000

    Add placeholder compoent for showig voting results
    Only header and button to clear votes currently.  Still need to
    actually show the results.

---
+++ components/VotingResult.tsx
@@ -0,0 +1,60 @@
+/*
+    Copyright 2021-2023 Rolf Michelsen and Tami Weiss
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+import React, { useContext, useState } from "react";
+
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
+import log from "loglevel";
+
+import Constants from "@/constants";
+import { SessionContext } from "@/model/context/SessionContext";
+import { resetVotesAsync } from "@/service/Service";
+
+
+// TODO Document this component
+export default function VotingResult(): JSX.Element
+{
+    const session = useContext(SessionContext);
+    const [ serviceErrorOpen, setServiceErrorOpen ] = useState<string|undefined>();         // Set when service error alert is visible.  Value is error message.
+
+    return (
+        <>
+            <Stack spacing={2} alignItems="center">
+                <Typography variant="h1">Voting results</Typography>
+                <Button variant="contained" onClick={startNextRound} >next round</Button>
+            </Stack>
+            <Snackbar open={serviceErrorOpen !== undefined} autoHideDuration={Constants.SnackbarDuration} onClose={() => setServiceErrorOpen(undefined)} anchorOrigin={Constants.SnackbarAnchor} >
+                <Alert severity="error">
+                    { serviceErrorOpen }
+                </Alert>
+            </Snackbar>
+        </>
+    );
+
+
+    /** Reset all votes to start a new voting round. */
+    function startNextRound(): void
+    {
+        resetVotesAsync(session.id!)
+            .then(() => log.info("Reset votes"))
+            .catch(() => setServiceErrorOpen("Unable to clear vote with remote service to start a new round."));
+    }
+}
