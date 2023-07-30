66b7e82ec29a809b902d5f3c4452aa12b8f2190c
Rolf Michelsen <rolf@rolfmichelsen.com>
2023-07-30 12:57:33 +0000

    Add component for visualizing voting participants
    The purpose of this component is to show the participants in a
    session and who has voted or not.

---
+++ components/VotingParticipants.tsx
@@ -0,0 +1,58 @@
+/*
+    Copyright 2021-2023 Rolf Michelsen and Tami Weiss
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+import React, { useContext } from "react";
+
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
+import Charm from "./Charm";
+import Participant from "@/model/Participant";
+import { SessionContext } from "@/model/context/SessionContext";
+
+
+/**
+ * Component to visualize the participants in a session and who has currently voted.
+ */
+export default function VotingParticipants(): JSX.Element
+{
+    const session = useContext(SessionContext);
+
+    // Function to return an avatar for a participant.
+    const getAvatar = (p: Participant) => (
+        <Stack alignItems="center">
+            <Charm charm={p.avatar.charm} size={64} />
+            <Typography>{p.avatar.name}</Typography>
+        </Stack>
+    );
+
+    return (
+        <Stack spacing={4} direction="row">
+            <Stack spacing={1} alignItems="left">
+                <Typography>Voted:</Typography>
+                <Stack direction="row">
+                    { session.participants.filter(p => p.vote != session.noVote?.id).map(p => getAvatar(p)) }
+                </Stack>
+            </Stack>
+            <Stack spacing={1} alignItems="left">
+                <Typography>Not yet voted:</Typography>
+                <Stack direction="row">
+                    { session.participants.filter(p => p.vote == session.noVote?.id).map(p => getAvatar(p)) }
+                </Stack>
+            </Stack>
+        </Stack>
+    );
+}
