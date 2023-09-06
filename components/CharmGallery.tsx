1bbf8aad06c5c1685eda9ce9d53de1146e5d4686
Rolf Michelsen <rolf@rolfmichelsen.com>
2023-09-06 18:47:33 +0000

    Center charms in charm gallery

--- components/CharmGallery.tsx
+++ components/CharmGallery.tsx
@@ -33,7 +33,7 @@ interface Props {
 export default function CharmGallery(props: Props): JSX.Element
 {
     return (
-        <Grid container spacing={2} >
+        <Grid container spacing={2} justifyContent="center" >
             { props.charms.charms.map(c =>
                 <Grid key={c} onClick={() => selectCharm(c)} >
                     <Charm charm={c} variant="small" />
