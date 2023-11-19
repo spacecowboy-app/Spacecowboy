8a9b5174d80577ad642503637a77b94074abece5
Rolf Michelsen <rolf@rolfmichelsen.com>
2023-11-19 16:24:48 +0000

    Add button to claim a not found session
    A bit of code duplication with the start page here.  Should look
    into consolidating that.

--- pages/[session]/notfound.tsx
+++ pages/[session]/notfound.tsx
@@ -14,6 +14,8 @@
     limitations under the License.
 */
 
+import { useContext } from "react";
+
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
@@ -21,7 +23,12 @@ import Typography from "@mui/material/Typography";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
+import log from "loglevel";
+
 import HeroImage from "@/components/HeroImage";
+import { createSessionAsync, getRandomSessionIdAsync, sessionIdExistsAsync } from "@/service/Service";
+import { SessionDispatchContext, clearSessionAction, setSessionIdAction, setSessionOwnerAction } from "@/model/context/SessionContext";
+import { storeSessionState } from "@/state/PersistentSessionState";
 
 import heroImage from "@/images/hero/noplace.png";
 
@@ -33,14 +40,48 @@ export default function SessionNotFound(): JSX.Element
 {
     const router = useRouter();
     const sessionId = router.query.session as string;
+    const dispatch = useContext(SessionDispatchContext);
 
     return (
         <Stack spacing={2} alignItems="center">
             <HeroImage src={heroImage} alt="" />
             <Typography variant="h3">The space {sessionId} does not exist</Typography>
             <Typography variant="h3">You can create it or join another</Typography>
-            <Button variant="contained" href="/" LinkComponent={Link}>go back</Button>
+            <Stack spacing={2} direction="row">
+                <Button variant="contained" href="/" LinkComponent={Link}>go back</Button>
+                <Button variant="contained" onClick={() => startSession() }>create it</Button>
+            </Stack>
         </Stack>
     );
 
+
+    /**
+     * Callback for starting a new session.
+     * Redirects to the session screen.
+     */
+    // TODO This code is duplicated from the start page. Should consolidate into a single instance.
+    function startSession(): void
+    {
+        if (sessionId) {
+            log.info(`Starting a new session ${sessionId}`);
+            createSessionAsync(sessionId)
+                .then(() => {
+                    dispatch(clearSessionAction());
+                    dispatch(setSessionIdAction(sessionId));
+                    dispatch(setSessionOwnerAction());
+                    storeSessionState({
+                        sessionId: sessionId,
+                        isOwner: true
+                    });
+                    router.push({ pathname:"/[session]", query: { session: sessionId } });
+                })
+                .catch((error) =>  {
+                    // TODO Proper error message in client window
+                    log.error(error);
+                });
+        }
+    }
+
 }
+
+
