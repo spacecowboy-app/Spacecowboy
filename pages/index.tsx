fe0dde41607aab00290b038d1f20035d2613e56b
Rolf Michelsen <rolf@rolfmichelsen.com>
2025-01-22 20:58:19 +0100

    Update copyright year

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 /*
-    Copyright 2021-2024 Rolf Michelsen and Tami Weiss
+    Copyright 2021-2025 Rolf Michelsen and Tami Weiss
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
